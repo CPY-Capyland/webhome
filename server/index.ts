ef673a2edf5da97f66ca7d444bc4b42dbe5e3982
Tado <contact@tadoniji.fr>
2025-12-09 20:03:01 +0100

    fix(auth): trust proxy in production for secure cookies

--- server/index.ts
+++ server/index.ts
@@ -10,6 +10,10 @@ const app = express();
 const httpServer = createServer(app);
 const PGStore = connectPgSimple(session);
 
+if (process.env.NODE_ENV === "production") {
+  app.set("trust proxy", 1);
+}
+
 declare module "http" {
   interface IncomingMessage {
     rawBody: unknown;
