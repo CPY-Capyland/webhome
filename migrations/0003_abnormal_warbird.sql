e24d68c647146f2e31b6739a53f43d1bd7a11ea9
Xx_Vessel69_xX <contact@tadoniji.fr>
2025-12-10 23:57:00 +0100

    fix: migration issue with null discord_id

--- migrations/0003_abnormal_warbird.sql
+++ migrations/0003_abnormal_warbird.sql
@@ -1,3 +1,4 @@
+DELETE FROM "users" WHERE "discord_id" IS NULL;--> statement-breakpoint
 ALTER TABLE "users" ALTER COLUMN "discord_id" SET NOT NULL;--> statement-breakpoint
 ALTER TABLE "houses" ADD COLUMN "last_color_changed_at" timestamp DEFAULT now() NOT NULL;--> statement-breakpoint
 ALTER TABLE "houses" ADD COLUMN "color" text DEFAULT '#FF0000' NOT NULL;
